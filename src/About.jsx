c7b3fc20d9fb0a5d81f0aae661bb550089ce3590
remco <remco@remcode.net>
2024-01-23 09:37:33 +0100

    test 80% height instead of 80vh

--- src/About.jsx
+++ src/About.jsx
@@ -3,7 +3,7 @@ import Card from "./Card"
 const About = ({selected,setSelected,section}) => {
   return (
     <div className={`cursor-pointer bg-[#556c37] fixed top-0 left-0 transition-all duration-500 
-                      ${selected === 1 ? "z-20 w-[100vw] h-[80vh] top-[10vh]" :
+                      ${selected === 1 ? "z-20 w-[100vw] h-[80%] top-[10vh]" :
                       selected === 2 ? "z-10 w-[100vw] h-[10vh]" :
                       selected === 0 ? "z-10 w-[50vw] h-[70vh]" :
                       "z-10 w-[50vw] h-[10vh]"}
